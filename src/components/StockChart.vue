96b4d2e0865fffcf3474f639566a52dfbbfe1b0e
inkexit <ryanmaelhorn@gmail.com>
2021-07-27 00:58:39 -0500

    Correct annotation of stockchart.vue

--- src/components/StockChart.vue
+++ src/components/StockChart.vue
@@ -87,13 +87,11 @@ const candle_up_color = '#fafafa'
 const candle_down_color = '#474747'
 
 // Create number formatter
+// https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-strings
+// Author: haykam
 const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD'
-
-    // Credit: haykam
-    // https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-strings
-
     // These options are needed to round to whole numbers if that's what you want.
     //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
     //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
